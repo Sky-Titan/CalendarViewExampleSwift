255b530802b11647739efd1877600d0ce779dd85
SkyTitan <er65119@naver.com>
2022-07-15 20:36:08 +0900

    Remove if statement

--- CalendarExample/CalendarView.swift
+++ CalendarExample/CalendarView.swift
@@ -45,18 +45,16 @@ class CalendarView: UIView {
     }
     
     private func loadNib() {
-        if let contentView = Bundle(for: CalendarView.self).loadNibNamed("CalendarView", owner: self)?.first as? UIView {
-            self.contentView = contentView
-            self.addSubview(contentView)
-            contentView.translatesAutoresizingMaskIntoConstraints = false
+        Bundle(for: CalendarView.self).loadNibNamed("CalendarView", owner: self)
+        self.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
             
-            NSLayoutConstraint.activate([
-                self.topAnchor.constraint(equalTo: contentView.topAnchor),
-                self.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-                self.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-                self.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
-            ])
-        }
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: contentView.topAnchor),
+            self.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            self.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
     }
     
     @IBAction func backgroundClicked(_ sender: Any) {
@@ -65,13 +63,13 @@ class CalendarView: UIView {
     
     func show() {
         self.isHidden = false
-        UIView.animate(withDuration: 0.2, delay: 0, options: [], animations: { [weak self] in
+        UIView.animate(withDuration: 0.15, delay: 0, options: [], animations: { [weak self] in
             self?.alpha = 1
         }, completion: nil)
     }
     
     func close() {
-        UIView.animate(withDuration: 0.2, delay: 0, options: [], animations: { [weak self] in
+        UIView.animate(withDuration: 0.15, delay: 0, options: [], animations: { [weak self] in
             self?.alpha = 0
         }, completion: { [weak self] _ in
             self?.removeFromSuperview()
